df892f5d651bd36a6a3d1de7cb2b2ab8f747e37a
dileep018kumar <79093720+dileep018kumar@users.noreply.github.com>
2021-07-20 10:37:16 +0530

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+template <typename T>
+class node
+{
+  ll id;
+  list<ll> neighbours;
+  list<T> inbox;
+  list<T> outbox;
+  vector<string> displayParameters;
+
+public:
+  ll stateVariables;
+  ll messageType;
+  vector<string> code;
+};
+int main()
+{
+  return 0;
+}
